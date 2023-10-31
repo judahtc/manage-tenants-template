a9caa480445800c857f7c3fa5e259f4f29bcf5f6
Carlos Tizirai <81642896+karlzw@users.noreply.github.com>
2023-10-31 17:24:31 +0200

    fix: read raw files

--- application/modeling/helper.py
+++ application/modeling/helper.py
@@ -118,7 +118,7 @@ def read_raw_file(
     project_id: int,
     boto3_session,
     file_name: Enum,
-    set_index: bool = False,
+    set_index: bool = True,
 ):
     try:
         df = wr.s3.read_parquet(
